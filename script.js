d030fa4e247f8f004535a3d9e1cbd2dc0a10dd18
izabellammg6 <izabellammg@hotmail.com>
2025-03-26 09:32:18 +0000

    update

--- script.js
+++ script.js
@@ -25,52 +25,117 @@ function checkAnswers() {
     }
 
     
-    if (answer1 === "1-3") score += 1;
-    else if (answer1 === "4-6") score += 2;
-    else if (answer1 === "7-8") score += 3;
-    else score += 4;
+    if (answer1 === "1-3") {
+        score += 1; 
+    }
+    else if (answer1 === "4-6") {
+        score += 2;
+    }
+    else if (answer1 === "7-8") {
+        score += 3;
+    }
+    else {
+        score += 4;
+    }
 
-    if (answer2 === "health") score += 1;
-    else if (answer2 === "family-friends") score += 2;
-    else if (answer2 === "expensive") score += 3;
-    else score += 4;
+    if (answer2 === "health") {
+        score += 1;
+    }
+    else if (answer2 === "family-friends"){
+         score += 2;
+    }
+    else if (answer2 === "expensive") {
+        score += 3;
+    }
+    else {
+         score += 4;
+    }
 
-    if (answer3 === "never") score += 1;
-    else if (answer3 === "once") score += 2;
-    else if (answer3 === "multiple") score += 3;
-    else score += 4;
+    if (answer3 === "never"){
+         score += 1;
+    }
+    else if (answer3 === "once") {
+        score += 2;
+    }
+    else if (answer3 === "multiple") {
+        score += 3;
+    }
+    else {
+        score += 4;
+    }
 
-    if (answer4 === "anxious") score += 1;
-    else if (answer4 === "neutral") score += 2;
-    else if (answer4 === "hopeful") score += 3;
-    else score += 4;
+    if (answer4 === "anxious"){
+         score += 1;
+    }
+    else if (answer4 === "neutral") {
+        score += 2;
+    }
+    else if (answer4 === "hopeful") {
+        score += 3;
+    }
+    else {
+        score += 4;
+    }
 
-    if (answer5 === "no-plan") score += 1;
-    else if (answer5 === "no-action") score += 2;
-    else if (answer5 === "plan-not-started") score += 3;
-    else score += 4;
+    if (answer5 === "no-plan") {
+        score += 1;
+    }
+    else if (answer5 === "no-action") {
+         score += 2;
+    }
+    else if (answer5 === "plan-not-started") {
+        score += 3;
+    }
+    else {
+        score += 4;
+    }
 
-    if (answer6 === "withdrawal") score += 1;
-    else if (answer6 === "stress-emotions") score += 2;
-    else if (answer6 === "social-situation") score += 3;
-    else score += 4;
+    if (answer6 === "withdrawal") {
+        score += 1;
+    }
+    else if (answer6 === "stress-emotions") {
+        score += 2;
+    }
+    else if (answer6 === "social-situation") {
+        score += 3;
+    }
+    else {
+        score += 4;
+    }
 
-    if (answer7 === "prefer-alone") score += 1;
-    else if (answer7 === "not-tried") score += 2;
-    else if (answer7 === "methods") score += 3;
-    else score += 4;
+    if (answer7 === "prefer-alone") {
+        score += 1;
+    }
+    else if (answer7 === "not-tried") {
+        score += 2;
+    }
+    else if (answer7 === "methods") {
+        score += 3;
+    }
 
-    if (answer8 === "fine-with") score += 1;
-    else if (answer8 === "sometimes") score += 2;
-    else if (answer8 === "fell-judged") score += 3;
-    else score += 4;
+    else {
+        score += 4;
+    }
+
+    if (answer8 === "fine-with") {
+        score += 1;
+    }
+    else if (answer8 === "sometimes") {
+        score += 2;
+    }
+    else if (answer8 === "fell-judged"){
+         score += 3;}
+
+    else {
+         score += 4;
+    }
 
     
-    if (score < 6) {
+    if (score < 10) {
         result = "You may not be ready to quit yet, but learning more about the benefits of quitting could help.";
-    } else if (score < 10) {
+    } else if (score < 20) {
         result = "You’re thinking about quitting, but you might need more motivation and a solid plan.";
-    } else if (score < 13) {
+    } else if (score < 30) {
         result = "You’re determined to quit and just need the right strategy and support.";
     } else {
         result = "You’re fully committed and ready to take action—go for it!";
